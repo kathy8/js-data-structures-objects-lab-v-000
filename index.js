6618df790336a40abb85b1243b0784adb8f3c43f
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-04-09 19:51:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
  driver[key] = value;
   return driver;
 };
+function deleteFromDriverByKey(driver, key) {
+ const newObj = Object.assign({}, driver);
+delete newObj[key];
+return newObj;
+};
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
+};
